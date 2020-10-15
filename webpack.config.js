c1b767867614577d460141be35a48e6f70dc7501
Mika Reich <mikareich0@gmail.com>
2020-10-15 21:50:48 +0200

    add eslint, prettier + create files

--- webpack.config.js
+++ webpack.config.js
@@ -1,23 +1,24 @@
-const HtmlWebpackPlugin = require("html-webpack-plugin");
-const path = require("path");
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const path = require('path')
 
 module.exports = {
+  entry: './src/app.js',
   module: {
     rules: [
       {
         test: /\.scss$/,
-        use: ["style-loader", "css-loader", "sass-loader"]
+        use: ['style-loader', 'css-loader', 'sass-loader'],
       },
       {
         test: /\.js$/,
         exclude: /node_modules/,
-        use: ["babel-loader"]
-      }
-    ]
+        use: ['babel-loader'],
+      },
+    ],
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: path.resolve(__dirname, "src", "index.html")
-    })
-  ]
-};
+      template: path.resolve(__dirname, 'src', 'index.html'),
+    }),
+  ],
+}
